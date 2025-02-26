02b5463b7b56e91c7603b093534fcd149d7e7b70
morison <frederickmorison@gmail.com>
2025-02-26 01:16:13 +0000

    project completed

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      let itemExist = state.items.find((item) => {
+        return item.name == name;
+      });
+
+      if (itemExist) {
+        itemExist.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      const { name } = action.payload;
+      state.items = state.items.filter((item) => {
+        item.name != name;
+      });
     },
     updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
 
-    
+      let itemExist = state.items.find((item) => {
+        return item.name == name;
+      });
+
+      if (itemExist && quantity > 0) {
+        itemExist.quantity = quantity;
+      }
     },
   },
 });
